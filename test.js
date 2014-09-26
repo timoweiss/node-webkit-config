e7596045f8c985ed4630eb2330b5ffa27af36d87
Timo Weiß <info@timo-weiss.com>
2014-09-26 11:56:41 +0200

    add test

--- test.js
+++ test.js
@@ -1,6 +1,15 @@
+'use strict';
+
 var assert = require('assert');
 var config = require('./index');
 
-describe('my test', function(){
-
+describe('my test', function() {
+    describe('config definitions', function() {
+        it('should be an function', function() {
+            assert.equal(typeof config, 'function');
+        });
+        it('should be an function', function() {
+            assert.equal(typeof config, 'function');
+        });
+    });
 });
