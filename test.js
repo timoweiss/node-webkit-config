b941e8b769378754b94bc0317fd75c7ac74dbcdf
Timo Weiß <info@timo-weiss.com>
2014-09-26 11:58:08 +0200

    add test, feeling better

--- test.js
+++ test.js
@@ -5,11 +5,17 @@ var config = require('./index');
 
 describe('my test', function() {
     describe('config definitions', function() {
-        it('should be an function', function() {
+        it('config should be an function', function() {
             assert.equal(typeof config, 'function');
         });
-        it('should be an function', function() {
-            assert.equal(typeof config, 'function');
+        it('config.get should be an function', function() {
+            assert.equal(typeof config.get, 'function');
+        });
+        it('config.set should be an function', function() {
+            assert.equal(typeof config.set, 'function');
+        });
+        it('config.setPath should be an function', function() {
+            assert.equal(typeof config.setPath, 'function');
         });
     });
 });
